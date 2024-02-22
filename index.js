72df7dffdaaaab17dbbffc48f9961ed1b1e2de4b
kezagiselle <kezagiselle776@gmail.com>
2024-02-22 16:15:41 +0200

    codes

---
+++ index.js
@@ -0,0 +1,17 @@
+//my age
+const myAge = 23;
+// earlyYears variable
+let earlyYears = 2;
+earlyYears *= 10.5;
+//subtracting myage into later years
+let laterYears = myAge - 2;
+//calculating the number of dog years
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+//my age in dog years
+let myAgeInDogYears = 21 + 84;
+//using built-in method to put myname in lowercase
+let myName ='KEZA'.toLowerCase();
+//the out put of my age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
